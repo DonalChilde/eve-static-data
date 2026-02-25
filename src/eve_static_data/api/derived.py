205b9f8ba6c4905aa425dc71d0e30e3c6867b549
Chad Lowe <pfmsoft.dev@gmail.com>
2026-02-25 04:50:23 -0700

    derived data lazy loader

--- src/eve_static_data/api/derived.py
+++ src/eve_static_data/api/derived.py
@@ -5,110 +5,64 @@ from eve_static_data.models.derived.market_path import MarketPathsDataset
 from eve_static_data.models.derived.normalized_eve_type import NormalizedEveTypesDataset
 from eve_static_data.models.derived.region_names import RegionNames
 from eve_static_data.models.derived.system_names import SystemNames
-from eve_static_data.sde_data.available_builds import path_to_derived
+from eve_static_data.settings import get_settings
 
 
-def market_paths(
-    build_number: int | None = None, localized: str = "en"
-) -> MarketPathsDataset:
-    """Get the file path for the market data derived dataset for a specific build number.
+class DerivedLazyLoader:
+    """A class for lazily loading derived datasets for a specific build number."""
 
-    Args:
-        build_number: The build number of the SDE data to read. If None, the most recent build will be used.
-        localized: The localization to use for the dataset. Defaults to "en".
+    def __init__(self, build_number: int | None = None, localized: str = "en"):
+        self.build_number = build_number
+        self.localized = localized
+        self.settings = get_settings()
+        if self.build_number is None:
+            self.build_number = self.settings.latest_build()
+        self.derived_path = self.settings.build_data_derived_dir(self.build_number)
+        self._market_paths: MarketPathsDataset | None = None
+        self._normalized_eve_types: NormalizedEveTypesDataset | None = None
+        self._normalized_eve_types_published: NormalizedEveTypesDataset | None = None
+        self._region_names: RegionNames | None = None
+        self._system_names: SystemNames | None = None
 
-    Returns:
-        An instance of MarketPathsDataset for the specified build number.
-    """
-    derived_path = path_to_derived(build_number)
-    dataset_path = derived_path / DerivedDatasetFiles.MARKET_PATHS.localized(localized)
-    if not dataset_path.exists():
-        raise FileNotFoundError(
-            f"Market paths dataset for build {build_number} not found at {dataset_path}"
-        )
-    return MarketPathsDataset.load_from_disk(dataset_path)
+    def market_paths(self) -> MarketPathsDataset:
+        if self._market_paths is None:
+            file_name = DerivedDatasetFiles.MARKET_PATHS.localized(self.localized)
+            dataset_path = self.derived_path / file_name
+            self._market_paths = MarketPathsDataset.load_from_disk(dataset_path)
+        return self._market_paths
 
+    def normalized_eve_types(self) -> NormalizedEveTypesDataset:
+        if self._normalized_eve_types is None:
+            file_name = DerivedDatasetFiles.NORMALIZED_EVE_TYPES.localized(
+                self.localized
+            )
+            dataset_path = self.derived_path / file_name
+            self._normalized_eve_types = NormalizedEveTypesDataset.load_from_disk(
+                dataset_path
+            )
+        return self._normalized_eve_types
 
-def normalized_eve_types(
-    build_number: int | None = None, localized: str = "en"
-) -> NormalizedEveTypesDataset:
-    """Get the file path for the normalized eve types derived dataset for a specific build number.
+    def normalized_eve_types_published(self) -> NormalizedEveTypesDataset:
+        if self._normalized_eve_types_published is None:
+            file_name = DerivedDatasetFiles.NORMALIZED_EVE_TYPES_PUBLISHED.localized(
+                self.localized
+            )
+            dataset_path = self.derived_path / file_name
+            self._normalized_eve_types_published = (
+                NormalizedEveTypesDataset.load_from_disk(dataset_path)
+            )
+        return self._normalized_eve_types_published
 
-    Args:
-        build_number: The build number of the SDE data to read. If None, the most recent build will be used.
-        localized: The localization to use for the dataset. Defaults to "en".
+    def region_names(self) -> RegionNames:
+        if self._region_names is None:
+            file_name = DerivedDatasetFiles.REGION_NAMES.localized(self.localized)
+            dataset_path = self.derived_path / file_name
+            self._region_names = RegionNames.load_from_disk(dataset_path)
+        return self._region_names
 
-    Returns:
-        An instance of NormalizedEveTypesDataset for the specified build number.
-    """
-    derived_path = path_to_derived(build_number)
-    dataset_path = derived_path / DerivedDatasetFiles.NORMALIZED_EVE_TYPES.localized(
-        localized
-    )
-    if not dataset_path.exists():
-        raise FileNotFoundError(
-            f"Normalized Eve types dataset for build {build_number} not found at {dataset_path}"
-        )
-    return NormalizedEveTypesDataset.load_from_disk(dataset_path)
-
-
-def normalized_eve_types_published(
-    build_number: int | None = None, localized: str = "en"
-) -> NormalizedEveTypesDataset:
-    """Get the file path for the normalized eve types published derived dataset for a specific build number.
-
-    Args:
-        build_number: The build number of the SDE data to read. If None, the most recent build will be used.
-        localized: The localization to use for the dataset. Defaults to "en".
-
-    Returns:
-        An instance of NormalizedEveTypesDataset for the specified build number.
-    """
-    derived_path = path_to_derived(build_number)
-    dataset_path = (
-        derived_path
-        / DerivedDatasetFiles.NORMALIZED_EVE_TYPES_PUBLISHED.localized(localized)
-    )
-    if not dataset_path.exists():
-        raise FileNotFoundError(
-            f"Normalized Eve types published dataset for build {build_number} not found at {dataset_path}"
-        )
-    return NormalizedEveTypesDataset.load_from_disk(dataset_path)
-
-
-def region_names(build_number: int | None = None, localized: str = "en") -> RegionNames:
-    """Get the file path for the region names derived dataset for a specific build number.
-
-    Args:
-        build_number: The build number of the SDE data to read. If None, the most recent build will be used.
-        localized: The localization to use for the dataset. Defaults to "en".
-
-    Returns:
-        An instance of RegionNames for the specified build number.
-    """
-    derived_path = path_to_derived(build_number)
-    dataset_path = derived_path / DerivedDatasetFiles.REGION_NAMES.localized(localized)
-    if not dataset_path.exists():
-        raise FileNotFoundError(
-            f"Region names dataset for build {build_number} not found at {dataset_path}"
-        )
-    return RegionNames.load_from_disk(dataset_path)
-
-
-def system_names(build_number: int | None = None, localized: str = "en") -> SystemNames:
-    """Get the file path for the system names derived dataset for a specific build number.
-
-    Args:
-        build_number: The build number of the SDE data to read. If None, the most recent build will be used.
-        localized: The localization to use for the dataset. Defaults to "en".
-
-    Returns:
-        An instance of SystemNames for the specified build number.
-    """
-    derived_path = path_to_derived(build_number)
-    dataset_path = derived_path / DerivedDatasetFiles.SYSTEM_NAMES.localized(localized)
-    if not dataset_path.exists():
-        raise FileNotFoundError(
-            f"System names dataset for build {build_number} not found at {dataset_path}"
-        )
-    return SystemNames.load_from_disk(dataset_path)
+    def system_names(self) -> SystemNames:
+        if self._system_names is None:
+            file_name = DerivedDatasetFiles.SYSTEM_NAMES.localized(self.localized)
+            dataset_path = self.derived_path / file_name
+            self._system_names = SystemNames.load_from_disk(dataset_path)
+        return self._system_names
