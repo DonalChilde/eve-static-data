5f1a33198ee4d6ac246aede521ae1e1082bee96a
Chad Lowe <pfmsoft.dev@gmail.com>
2026-03-30 06:14:27 -0700

    functions to export noramlized types and market paths to csv

--- scripts/exports.py
+++ scripts/exports.py
@@ -9,6 +9,8 @@ from typing import Annotated
 from typer import Typer
 
 from eve_static_data import ESDLoader
+from eve_static_data.exports.eve_types_table import export_eve_types_table
+from eve_static_data.exports.market_paths import export_market_paths
 from eve_static_data.logging_config import setup_logging
 
 logger = logging.getLogger(__name__)
@@ -20,9 +22,8 @@ app = Typer(no_args_is_help=True)
 @app.command()
 def export(sde_path: Annotated[Path, "Path to the SDE directory"]):
     """Export the SDE data to JSON format."""
-    loader = ESDLoader(
-        sde_path=sde_path, derived_datasets_path=sde_path / "derived_datasets"
-    )
+    derived_path = sde_path / "derived_datasets"
+    loader = ESDLoader(sde_path=sde_path, derived_datasets_path=derived_path)
     # eve_types_localized = loader.localized_datasets.eve_types()
     # print(f"Exported {len(eve_types_localized.records)} EVE types.")
 
@@ -34,6 +35,28 @@ def export(sde_path: Annotated[Path, "Path to the SDE directory"]):
     print(f"Exported {len(region_names.records)} region names.")
     system_names = loader.derived_datasets.system_names()
     print(f"Exported {len(system_names.records)} system names.")
+
+    # -----------------------------------------------------------------
+    # After loading the datasets, we can export them to csv files.
+    # -----------------------------------------------------------------
+    exports_path = sde_path / "exports"
+
+    types_table_filepath = exports_path / "eve_types_table.csv"
+    rows_written = export_eve_types_table(
+        market_paths=market_paths,
+        normalized_eve_types=eve_types,
+        output_file=types_table_filepath,
+        overwrite=True,
+    )
+    print(f"Wrote {rows_written} rows to {types_table_filepath}.")
+
+    market_paths_filepath = exports_path / "market_paths.csv"
+    rows_written = export_market_paths(
+        market_paths=market_paths,
+        output_file=market_paths_filepath,
+        overwrite=True,
+    )
+    print(f"Wrote {rows_written} rows to {market_paths_filepath}.")
 
 
 if __name__ == "__main__":
