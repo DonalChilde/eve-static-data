ef42209a6949bc00583131adecd2bca68ea5f284
Chad Lowe <pfmsoft.dev@gmail.com>
2026-04-27 12:52:09 -0700

    comment out derived code

--- src/eve_static_data/models/derived/__init__.py
+++ src/eve_static_data/models/derived/__init__.py
@@ -1,13 +1,13 @@
 """Derived models for EVE Static Data Export (SDE) data."""
 
-from eve_static_data.models.derived.market_path import MarketPathsDataset
-from eve_static_data.models.derived.normalized_eve_type import NormalizedEveTypesDataset
-from eve_static_data.models.derived.region_names import RegionNames
-from eve_static_data.models.derived.system_names import SystemNames
+# from eve_static_data.models.derived.market_path import MarketPathsDataset
+# from eve_static_data.models.derived.normalized_eve_type import NormalizedEveTypesDataset
+# from eve_static_data.models.derived.region_names import RegionNames
+# from eve_static_data.models.derived.system_names import SystemNames
 
-__all__ = [
-    "MarketPathsDataset",
-    "NormalizedEveTypesDataset",
-    "RegionNames",
-    "SystemNames",
-]
+# __all__ = [
+#     "MarketPathsDataset",
+#     "NormalizedEveTypesDataset",
+#     "RegionNames",
+#     "SystemNames",
+# ]
